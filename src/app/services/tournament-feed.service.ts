023875fbe2ad3febfc61cdffdf9889418a72f919
Rich <rich@learningontheroad.co.uk>
2016-11-27 22:04:16 +0000

    switch googlemaps to http

--- src/app/services/tournament-feed.service.ts
+++ src/app/services/tournament-feed.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 
 import { STATIC_TOURNAMENTS } from '../models/static-tournament';
 
-export const GEOLOCATION_BASE_URL = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+export const GEOLOCATION_BASE_URL = "http://maps.googleapis.com/maps/api/geocode/json?address=";
 export const TEMPERATURE_BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
 
 const smallViewPort = window.matchMedia("(max-device-width: 640px)");
